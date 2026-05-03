08b8820621882e859222d3e5b213039967619b71
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-04 01:14:24 +0900

    Wire optional node key validation into Tree

--- lib/tree_view.rb
+++ lib/tree_view.rb
@@ -7,10 +7,13 @@ require "tree_view/render_state"
 require "tree_view/toggle_scope"
 require "tree_view/traversal"
 require "tree_view/tree"
+require "tree_view/node_key_validation"
 require "tree_view/path_tree"
 require "tree_view/reverse_tree"
 require "tree_view/ui_config"
 require "tree_view/ui_config_builder"
+
+TreeView::Tree.prepend(TreeView::NodeKeyValidation)
 
 module TreeView
   class << self
