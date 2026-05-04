be23d17d2c1193dafce1f622169c2bcd5b7266a3
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 06:27:03 +0900

    Rename state owner helpers to tree_instance_key

--- lib/generators/tree_view/state/templates/tree_view_state_owner.rb
+++ lib/generators/tree_view/state/templates/tree_view_state_owner.rb
@@ -7,15 +7,15 @@ module TreeViewStateOwner
     has_many :tree_view_states, as: :owner, dependent: :destroy
   end
 
-  def tree_view_state_for(view_key)
-    record = tree_view_states.find_by(view_key: view_key)
-    TreeView::PersistedState.new(view_key: view_key, expanded_keys: record&.expanded_keys || [])
+  def tree_view_state_for(tree_instance_key)
+    record = tree_view_states.find_by(tree_instance_key: tree_instance_key)
+    TreeView::PersistedState.new(tree_instance_key: tree_instance_key, expanded_keys: record&.expanded_keys || [])
   end
 
-  def save_tree_view_state!(view_key, expanded_keys:)
-    record = tree_view_states.find_or_initialize_by(view_key: view_key)
+  def save_tree_view_state!(tree_instance_key, expanded_keys:)
+    record = tree_view_states.find_or_initialize_by(tree_instance_key: tree_instance_key)
     record.expanded_keys = Array(expanded_keys)
     record.save!
-    TreeView::PersistedState.new(view_key: view_key, expanded_keys: record.expanded_keys)
+    TreeView::PersistedState.new(tree_instance_key: tree_instance_key, expanded_keys: record.expanded_keys)
   end
 end
