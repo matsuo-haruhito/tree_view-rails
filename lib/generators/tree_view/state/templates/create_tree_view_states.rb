32bd33b2bd6f1c3d5df06152841840eeba12fe14
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 06:23:55 +0900

    Rename state migration column to tree_instance_key

--- lib/generators/tree_view/state/templates/create_tree_view_states.rb
+++ lib/generators/tree_view/state/templates/create_tree_view_states.rb
@@ -4,11 +4,11 @@ class CreateTreeViewStates < ActiveRecord::Migration[7.1]
   def change
     create_table :tree_view_states do |t|
       t.references :owner, polymorphic: true, null: false
-      t.string :view_key, null: false
+      t.string :tree_instance_key, null: false
       t.json :expanded_keys, null: false, default: []
       t.timestamps
     end
 
-    add_index :tree_view_states, [:owner_type, :owner_id, :view_key], unique: true
+    add_index :tree_view_states, [:owner_type, :owner_id, :tree_instance_key], unique: true
   end
 end
