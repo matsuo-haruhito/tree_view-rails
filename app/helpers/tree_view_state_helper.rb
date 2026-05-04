61f2435229e6d0f582a57d75f42bbf5f3e7ca887
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 06:47:01 +0900

    Expose selection controller data from render state

--- app/helpers/tree_view_state_helper.rb
+++ app/helpers/tree_view_state_helper.rb
@@ -3,12 +3,21 @@
 module TreeViewStateHelper
   def tree_view_state_data(render_state)
     controllers = ["tree-view-state"]
+    controllers << "tree-view-selection" if render_state.selection_enabled?
     controllers << "tree-view-transfer" if render_state.respond_to?(:row_event_payload_builder) && render_state.row_event_payload_builder
 
     data = { controller: controllers.join(" ") }
     if render_state.respond_to?(:tree_instance_key) && render_state.tree_instance_key.present?
       data[:tree_view_state_tree_instance_key_value] = render_state.tree_instance_key
     end
+
+    if render_state.selection_enabled?
+      data[:action] = append_tree_view_action(data[:action], "change->tree-view-selection#toggle")
+      data[:tree_view_selection_cascade_value] = true if render_state.selection_cascade?
+      data[:tree_view_selection_indeterminate_value] = true if render_state.selection_indeterminate?
+      data[:tree_view_selection_max_count_value] = render_state.selection_max_count if render_state.selection_max_count
+    end
+
     data
   end
 
@@ -18,6 +27,14 @@ module TreeViewStateHelper
       tree_view_state_node_key: tree.node_key_for(item),
       tree_view_state_expanded: expanded
     }
+  end
+
+  private
+
+  def append_tree_view_action(existing_action, action)
+    actions = existing_action.to_s.split
+    actions << action unless actions.include?(action)
+    actions.join(" ")
   end
 end
 
