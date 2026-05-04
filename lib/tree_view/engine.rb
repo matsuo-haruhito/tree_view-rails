16df9125d5a8eb4706a53480fbdd6137ed87cda8
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 01:20:36 +0900

    fix: load row actions helper before engine registration

--- lib/tree_view/engine.rb
+++ lib/tree_view/engine.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails/engine"
+require_relative File.join("..", "..", "app", "helpers", "tree_view_row_actions_helper")
 
 module TreeView
   class Engine < ::Rails::Engine
