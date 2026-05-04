1885ece1ffb389660be34f0e202adf88968a289a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-05 05:31:44 +0900

    test: cover transfer controller root data

--- spec/helpers/tree_view_state_helper_spec.rb
+++ spec/helpers/tree_view_state_helper_spec.rb
@@ -10,20 +10,27 @@ RSpec.describe TreeViewStateHelper do
   end
 
   it "builds root data without a view key" do
-    state = instance_double(TreeView::RenderState, view_key: nil)
+    state = instance_double(TreeView::RenderState, view_key: nil, row_event_payload_builder: nil)
     helper = helper_host_class.new
 
     expect(helper.tree_view_state_data(state)).to eq(controller: "tree-view-state")
   end
 
   it "builds root data with a view key" do
-    state = instance_double(TreeView::RenderState, view_key: "documents")
+    state = instance_double(TreeView::RenderState, view_key: "documents", row_event_payload_builder: nil)
     helper = helper_host_class.new
 
     expect(helper.tree_view_state_data(state)).to eq(
       controller: "tree-view-state",
       tree_view_state_view_key_value: "documents"
     )
+  end
+
+  it "adds the transfer controller when row event payloads are configured" do
+    state = instance_double(TreeView::RenderState, view_key: nil, row_event_payload_builder: ->(_item) { {} })
+    helper = helper_host_class.new
+
+    expect(helper.tree_view_state_data(state)).to eq(controller: "tree-view-state tree-view-transfer")
   end
 
   it "builds row state data" do
