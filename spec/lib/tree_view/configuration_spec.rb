40b2391ff9abb50a7e80f5eae50c2658921eef26
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-08 08:48:52 +0900

    Test render log level configuration

--- spec/lib/tree_view/configuration_spec.rb
+++ spec/lib/tree_view/configuration_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "logger"
 
 RSpec.describe TreeView::Configuration do
   describe "#initial_state=" do
@@ -21,6 +22,45 @@ RSpec.describe TreeView::Configuration do
 
       expect { config.initial_state = nil }.to raise_error(ArgumentError, /initial_state must be one of/)
       expect { config.initial_state = 1 }.to raise_error(ArgumentError, /initial_state must be one of/)
+    end
+  end
+
+  describe "#render_log_level=" do
+    it "defaults to warn" do
+      config = described_class.new
+
+      expect(config.render_log_level).to eq(:warn)
+    end
+
+    it "accepts logger level names" do
+      config = described_class.new
+
+      config.render_log_level = :info
+
+      expect(config.render_log_level).to eq(:info)
+    end
+
+    it "accepts logger level constants" do
+      config = described_class.new
+
+      config.render_log_level = Logger::ERROR
+
+      expect(config.render_log_level).to eq(:error)
+    end
+
+    it "accepts nil to disable TreeView render log silencing" do
+      config = described_class.new
+
+      config.render_log_level = nil
+
+      expect(config.render_log_level).to be_nil
+    end
+
+    it "rejects invalid values" do
+      config = described_class.new
+
+      expect { config.render_log_level = :verbose }.to raise_error(ArgumentError, /render_log_level/)
+      expect { config.render_log_level = Object.new }.to raise_error(ArgumentError, /render_log_level/)
     end
   end
 end
