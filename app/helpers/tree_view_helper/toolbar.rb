b02e88ef7c2d7f7c6be477a99289eff0377ce3b9
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-26 04:13:49 +0900

    feat: add toolbar action metadata helpers

--- app/helpers/tree_view_helper/toolbar.rb
+++ app/helpers/tree_view_helper/toolbar.rb
@@ -16,38 +16,56 @@ module TreeViewHelper
     }.freeze
 
     def tree_view_toolbar(render_state, actions: DEFAULT_TREE_VIEW_TOOLBAR_ACTIONS, labels: {}, class_name: "tree-view-toolbar", button_class_name: "tree-view-toolbar__button")
-      normalized_actions = normalize_tree_view_toolbar_actions(actions)
-      normalized_labels = labels.to_h.transform_keys(&:to_sym)
-
       content_tag(:div, class: class_name, data: {tree_view_toolbar: true}) do
         safe_join(
-          normalized_actions.filter_map do |action|
-            tree_view_toolbar_action(render_state, action, normalized_labels, button_class_name)
+          tree_view_toolbar_actions(render_state, actions: actions, labels: labels).map do |action|
+            tree_view_toolbar_action_tag(action, button_class_name)
           end
         )
       end
     end
 
-    private
+    def tree_view_toolbar_actions(render_state, actions: DEFAULT_TREE_VIEW_TOOLBAR_ACTIONS, labels: {})
+      normalized_labels = labels.to_h.transform_keys(&:to_sym)
 
-    def normalize_tree_view_toolbar_actions(actions)
-      Array(actions).map do |action|
-        normalized_action = action.to_sym
-        next normalized_action if TREE_VIEW_TOOLBAR_ACTIONS.include?(normalized_action)
-
-        raise TreeView::ConfigurationError, "unknown tree_view_toolbar action: #{action}; supported actions are: #{TREE_VIEW_TOOLBAR_ACTIONS.join(", ")}"
+      normalize_tree_view_toolbar_actions(actions).map do |action|
+        tree_view_toolbar_action_metadata(render_state, action, label: normalized_labels[action])
       end
     end
 
-    def tree_view_toolbar_action(render_state, action, labels, button_class_name)
-      path = tree_view_toolbar_path(render_state, action)
-      label = labels.fetch(action, TREE_VIEW_TOOLBAR_LABELS.fetch(action))
-      data = {tree_view_toolbar_action: action}
+    def tree_view_toolbar_action_metadata(render_state, action, label: nil)
+      normalized_action = normalize_tree_view_toolbar_action(action)
+      path = tree_view_toolbar_path(render_state, normalized_action)
+      disabled = path.nil?
 
-      if path
-        link_to(label, path, class: button_class_name, data: data)
+      {
+        action: normalized_action,
+        state: TREE_VIEW_TOOLBAR_STATES.fetch(normalized_action),
+        label: label || TREE_VIEW_TOOLBAR_LABELS.fetch(normalized_action),
+        path: path,
+        disabled: disabled,
+        data: tree_view_toolbar_action_data(normalized_action, disabled: disabled)
+      }
+    end
+
+    private
+
+    def normalize_tree_view_toolbar_actions(actions)
+      Array(actions).map { |action| normalize_tree_view_toolbar_action(action) }
+    end
+
+    def normalize_tree_view_toolbar_action(action)
+      normalized_action = action.to_sym
+      return normalized_action if TREE_VIEW_TOOLBAR_ACTIONS.include?(normalized_action)
+
+      raise TreeView::ConfigurationError, "unknown tree_view_toolbar action: #{action}; supported actions are: #{TREE_VIEW_TOOLBAR_ACTIONS.join(", ")}"
+    end
+
+    def tree_view_toolbar_action_tag(action, button_class_name)
+      if action[:path]
+        link_to(action[:label], action[:path], class: button_class_name, data: action[:data])
       else
-        button_tag(label, type: "button", class: button_class_name, disabled: true, data: data.merge(tree_view_toolbar_disabled: true))
+        button_tag(action[:label], type: "button", class: button_class_name, disabled: true, data: action[:data])
       end
     end
 
@@ -55,6 +73,13 @@ module TreeViewHelper
       return nil unless render_state.ui_config.respond_to?(:toggle_all_path)
 
       render_state.ui_config.toggle_all_path(state: TREE_VIEW_TOOLBAR_STATES.fetch(action))
+    end
+
+    def tree_view_toolbar_action_data(action, disabled:)
+      {
+        tree_view_toolbar_action: action,
+        tree_view_toolbar_disabled: disabled || nil
+      }.compact
     end
   end
 end
