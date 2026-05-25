f8765cea241f7b2266de2caae1bcabc9b9a677a8
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-25 09:14:35 +0900

    feat: add lazy loading placeholder helpers

--- app/helpers/tree_view_helper/lazy_loading.rb
+++ app/helpers/tree_view_helper/lazy_loading.rb
@@ -1,5 +1,20 @@
 module TreeViewHelper
   module LazyLoading
+    def tree_children_container_dom_id(item, ui: @tree_ui)
+      "#{tree_node_dom_id(item, ui: ui)}_children"
+    end
+
+    def tree_remote_state_placeholder_dom_id(item, ui: @tree_ui)
+      "#{tree_node_dom_id(item, ui: ui)}_remote_state"
+    end
+
+    def tree_remote_state_placeholder_attributes(item, state: nil, ui: @tree_ui)
+      attributes = { id: tree_remote_state_placeholder_dom_id(item, ui: ui) }
+      return attributes if state.nil?
+
+      attributes.merge(data: { tree_remote_state: state.to_s })
+    end
+
     def tree_lazy_loading_data(item, tree, render_context, depth:)
       path = tree_load_children_path(item, depth, scope: render_context.lazy_loading_scope, ui: render_context.render_state.ui_config)
       return {} if path.nil?
