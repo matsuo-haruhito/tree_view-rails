b9eee090bf070b12e1deea9ec2c68d6b8fd2daff
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-03 20:14:26 +0900

    test: satisfy standardrb for toolbar option spec

--- spec/public_api_toolbar_helper_options_spec.rb
+++ spec/public_api_toolbar_helper_options_spec.rb
@@ -3,31 +3,38 @@
 require "spec_helper"
 require "yaml"
 
-RSpec.describe "Toolbar helper option public contract" do
-  PUBLIC_API_MANIFEST_PATH = File.expand_path("../config/public_api_manifest.yml", __dir__)
-  EXPECTED_TREE_VIEW_TOOLBAR_OPTION_KEYS = %w[
-    actions
-    labels
-    class_name
-    button_class_name
-    html
-    action_html
-  ].freeze
+PUBLIC_API_TOOLBAR_HELPER_OPTIONS_MANIFEST_PATH = File.expand_path("../config/public_api_manifest.yml", __dir__)
+EXPECTED_TREE_VIEW_TOOLBAR_OPTION_KEYS = %w[
+  actions
+  labels
+  class_name
+  button_class_name
+  html
+  action_html
+].freeze
 
+RSpec.describe "Toolbar helper option public contract" do
   def public_api_manifest
-    @public_api_manifest ||= YAML.safe_load_file(PUBLIC_API_MANIFEST_PATH)
+    @public_api_manifest ||= YAML.safe_load_file(PUBLIC_API_TOOLBAR_HELPER_OPTIONS_MANIFEST_PATH)
+  end
+
+  def tree_view_toolbar_keyword_options
+    TreeViewHelper::Toolbar
+      .instance_method(:tree_view_toolbar)
+      .parameters
+      .filter_map do |kind, name|
+        case kind
+        when :key, :keyreq
+          name.to_s
+        end
+      end
   end
 
   it "keeps tree_view_toolbar keyword options in the public API manifest" do
     manifest_option_keys = public_api_manifest.fetch("helper_option_keys").fetch("tree_view_toolbar")
-    helper_keyword_options = TreeViewHelper::Toolbar
-      .instance_method(:tree_view_toolbar)
-      .parameters
-      .select { |kind, _name| %i[key keyreq].include?(kind) }
-      .map { |_kind, name| name.to_s }
 
     expect(manifest_option_keys).to eq(EXPECTED_TREE_VIEW_TOOLBAR_OPTION_KEYS)
-    expect(manifest_option_keys).to eq(helper_keyword_options)
+    expect(manifest_option_keys).to eq(tree_view_toolbar_keyword_options)
   end
 
   it "keeps helper option keys separate from toolbar action state mapping" do
