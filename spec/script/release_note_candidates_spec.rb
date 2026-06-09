25d2f070e7784e92c02db53b97f82b5f3192760e
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-09 13:15:52 +0900

    fix: move fake release client out of spec block

--- spec/script/release_note_candidates_spec.rb
+++ spec/script/release_note_candidates_spec.rb
@@ -3,66 +3,66 @@
 require "spec_helper"
 require_relative "../../script/release_note_candidates"
 
-RSpec.describe ReleaseNoteCandidates::Collector do
-  class FakeReleaseNoteClient
-    attr_reader :queries
+class FakeReleaseNoteClient
+  attr_reader :queries
 
-    def initialize
-      @queries = []
-    end
+  def initialize
+    @queries = []
+  end
 
-    def search(query)
-      @queries << query
-      if query.include?("is:pr")
-        [
-          {
-            "number" => 12,
-            "title" => "Add toolbar helper",
-            "html_url" => "https://github.com/example/repo/pull/12",
-            "pull_request" => {"merged_at" => "2026-06-01T00:00:00Z"}
-          }
-        ]
-      else
-        [
-          {
-            "number" => 34,
-            "title" => "Clarify release docs",
-            "html_url" => "https://github.com/example/repo/issues/34",
-            "closed_at" => "2026-06-02T00:00:00Z"
-          }
-        ]
-      end
-    end
-
-    def compare(_repo, _base_ref)
-      {
-        "commits" => [
-          {"commit" => {"message" => "Merge pull request #12 from feature\n\nCloses #34"}},
-          {"commit" => {"message" => "Docs update without issue reference"}}
-        ]
-      }
-    end
-
-    def issue(_repo, number)
-      case number.to_i
-      when 12
+  def search(query)
+    @queries << query
+    if query.include?("is:pr")
+      [
         {
           "number" => 12,
           "title" => "Add toolbar helper",
           "html_url" => "https://github.com/example/repo/pull/12",
           "pull_request" => {"merged_at" => "2026-06-01T00:00:00Z"}
         }
-      when 34
+      ]
+    else
+      [
         {
           "number" => 34,
           "title" => "Clarify release docs",
           "html_url" => "https://github.com/example/repo/issues/34",
           "closed_at" => "2026-06-02T00:00:00Z"
         }
-      end
+      ]
     end
   end
 
+  def compare(_repo, _base_ref)
+    {
+      "commits" => [
+        {"commit" => {"message" => "Merge pull request #12 from feature\n\nCloses #34"}},
+        {"commit" => {"message" => "Docs update without issue reference"}}
+      ]
+    }
+  end
+
+  def issue(_repo, number)
+    case number.to_i
+    when 12
+      {
+        "number" => 12,
+        "title" => "Add toolbar helper",
+        "html_url" => "https://github.com/example/repo/pull/12",
+        "pull_request" => {"merged_at" => "2026-06-01T00:00:00Z"}
+      }
+    when 34
+      {
+        "number" => 34,
+        "title" => "Clarify release docs",
+        "html_url" => "https://github.com/example/repo/issues/34",
+        "closed_at" => "2026-06-02T00:00:00Z"
+      }
+    end
+  end
+end
+
+RSpec.describe ReleaseNoteCandidates::Collector do
   it "collects merged pull requests and closed issues for a date window" do
     client = FakeReleaseNoteClient.new
     merged_prs, closed_issues = described_class.new(client: client).by_since_date(
