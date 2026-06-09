5d0f4d2a3f12d9ac9e3428a4c88506baed72ae6f
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-09 15:25:14 +0900

    test: sync manifest structure keys

--- spec/public_api_manifest_structure_spec.rb
+++ spec/public_api_manifest_structure_spec.rb
@@ -10,12 +10,15 @@ PUBLIC_API_MANIFEST_TOP_LEVEL_KEYS = %w[
   module_methods
   configuration_options
   public_constants
+  graph_adapter_initializer
   path_tree_builder_node_shapes
   helper_methods
   helper_option_keys
   toolbar_actions
+  toolbar_action_metadata
   setup_generators
   grouped_option_keys
+  diagnostics
   resource_table_render_state_call
   render_state_callback_builder_keys
   javascript_package_root
